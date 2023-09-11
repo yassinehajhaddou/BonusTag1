06eb6636f5e3ab711f497cfcf93976c7ae43465d
yassinehajhaddou <yassinehajhaddou@gmail.com>
2023-09-11 14:53:18 +0200

    Bonusaufgabe abgeschlossen.

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,15 @@
+fun main(args: Array<String>) {
+
+    // Liest den Namen ein, und begrüßt den Nutzer
+    println("Hallo, wie heißt du? (Bestätitige mithilfe von Enter)")
+    var name = readln()
+    println("Hallo $name!")
+
+    // Druckt eine leere Zeile aus
+    println()
+
+    // Liest das Alter ein. Zudem ist dieses Symbol " $ " ein Adressierer, um die Variable "alter" Zuortbar zu machen.
+    println("Wie alt bist du? (Bestätitige mithilfe von Enter)")
+    var alter = readln()    // Diese Zeile liest das Alter ein
+    println("Wow! Bereits $alter Jahre?!")
+}
